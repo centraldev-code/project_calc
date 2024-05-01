2291dd1e7566ad65a8c2fd80e0965abe0b75f0ae
Central Dev <centraldev@outlook.com.br>
2024-04-30 21:55:23 -0300

    Melhorias nas funcionalidades

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 const display = document.getElementById("display");
-const subDisplay = document.getElementById("subDisplay")
-console.log(display.innerText);
-
-subDisplay.innerText = "";
 display.innerText = 0;
 
 let acao = false;
 let resultado = 0;
 let resultadoSub = 0;
 let nameAcao = "";
-
+let virgula = false;
 
 function addNumber(number) {
-
-    if(display.innerText == 0 || acao) {
+    if (display.innerText == 0 || acao) {
         display.innerText = number
     } else {
         display.innerText += number
@@ -22,87 +17,81 @@ function addNumber(number) {
 }
 //  = = igual  ! = diferente
 function deleted() {
-
     const tamanho = display.innerText.length;
-    if(display.innerText.length != 1) {
+    if (display.innerText.length != 1) {
         display.innerText = display.innerText.substring(0, (tamanho - 1))
     } else {
         display.innerText = 0;
     }
-
 }
 
 function operacao(params) {
-    console.log(params);
     nameAcao = params;
-    if(params == "adicao") {
-        acao = true;
-        resultado = parseInt(resultado) + parseInt(display.innerText);
-        addOperacaoSubDisplay(params);
-        console.log(resultado);
+    acao = true;
+    if (params == "adicao") resultado = parseFloat(resultado) + parseFloat(display.innerText);
+    if (params == "subtracao") resultado = parseFloat(resultado) - parseFloat(display.innerText);
+    if (params == "divisao") {
+        if (resultado != 0) {
+            resultado = parseFloat(resultado) / parseFloat(display.innerText);
+        } else {
+            resultado = parseFloat(display.innerText);
+            return;
+        }
     }
-    if(params == "subtracao") {
-        acao = true;
-        resultado = parseInt(resultado) - parseInt(display.innerText);
-        addOperacaoSubDisplay(params);
-        console.log(resultado);
-    }
-    if(params == "divisao") {
-        
-    }
-    if(params == "multiplicao") {
-        
-    }
-    
-}
+    if (params == "multiplicacao") {
+        if (resultado != 0) {
+            resultado = parseFloat(resultado) * parseFloat(display.innerText);
+        } else {
+            resultado = parseFloat(display.innerText);
+            return;
+        }
 
-function addOperacaoSubDisplay(params) {
-    let simbolo = "+";
-
-    if(params == "subtracao") {
-        simbolo = "-";
-    }
-    if(params == "multiplicao") {
-        simbolo = "*";
-    }
-    if(params == "divisao") {
-        simbolo = "/";
-    }
-    if(params == "igual") {
-        simbolo = "=";
-        subDisplay.innerText = resultado + " + " + display.innerText + " " + simbolo;
-        return;
-    }
-    
-    subDisplay.innerText = resultado + " " + simbolo;
-    
-}
-
-function result() {
-    addOperacaoSubDisplay("igual")
-    if(nameAcao == "adicao") {
-        resultado = parseInt(resultado) + parseInt(display.innerText);
     }
     display.innerText = resultado;
-    console.log(resultado);
-    //display.style.backgroundColor = "red";
+    virgula = false;
 }
 
-function updateResult(operador) {
-    if (operador == "adicao") {
-        resultado = parseInt(resultado) + parseInt(display.innerText);
+
+function result() {
+    acao = true;
+    switch (nameAcao) {
+        case "adicao":
+            resultado = parseFloat(resultado) + parseFloat(display.innerText);
+            break;
+        case "subtracao":
+            resultado = parseFloat(resultado) - parseFloat(display.innerText);
+            break;
+        case "divisao":
+            resultado = parseFloat(resultado) / parseFloat(display.innerText);
+            break;
+        case "multiplicacao":
+            resultado = parseFloat(resultado) * parseFloat(display.innerText);
+            break;
+        default:
+            break;
     }
-    
+    display.innerText = resultado;
+    nameAcao = "";
+    virgula = false;
+
+}
+
+function addVirgula() {
+    if (!virgula)
+        display.innerText += ".";
+
+    virgula = true;
 }
 
 function ce() {
     display.innerText = 0;
+    virgula = false;
 }
 
 function reset() {
     display.innerText = 0;
-    subDisplay.innerText = "";
     resultado = 0;
     nameAcao = "";
+    acao = false;
+    virgula = false;
 }
-
